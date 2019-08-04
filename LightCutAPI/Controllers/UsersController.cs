8e503a53d5df6558c5b6b73f52aa46e2b37a5d31
Hugo Dias <hmdias@gmail.com>
2019-08-04 22:51:26 +0100

    Add Users controller and service

--- LightCutAPI/Controllers/UsersController.cs
+++ LightCutAPI/Controllers/UsersController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using LightCutAPI.Models;
 using LightCutAPI.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace LightCutAPI.Controllers
 {
@@ -13,5 +10,69 @@ namespace LightCutAPI.Controllers
     public class UsersController : ControllerBase
     {
         private readonly UserService _service;
+
+        public UsersController(UserService service)
+        {
+            this._service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<List<User>> Get()
+        {
+            return _service.Get();
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetUser")]
+        public ActionResult<User> Get(string id)
+        {
+            var user = _service.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        public ActionResult<User> Create(User user)
+        {
+            _service.Create(user);
+
+            return CreatedAtRoute("GetUser", new { id = user.Id.ToString() }, user);
+        }
+
+
+        [HttpPut("{id:length(24)}")]
+        public ActionResult Update(string id, User userIn)
+        {
+            var user = _service.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _service.Update(id, userIn);
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id:length(24)}")]
+        public ActionResult Remove(string id)
+        {
+            var user = _service.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _service.Remove(id);
+
+            return NoContent();
+        }
     }
 }
--- LightCutAPI/Services/UserService.cs
+++ LightCutAPI/Services/UserService.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using LightCutAPI.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace LightCutAPI.Services
 {
     public class UserService
     {
+        private readonly IMongoCollection<User> _collection;
+
+        public UserService(IConfiguration config)
+        {
+            var mongoClient = new MongoClient(config.GetConnectionString("LightcutDB"));
+            var database = mongoClient.GetDatabase(config["Database"]);
+            this._collection = database.GetCollection<User>("users");
+        }
+
+        public List<User> Get()
+        {
+            return _collection.Find(user => true).ToList();
+        }
+
+        public User Get(string id)
+        {
+            return _collection.Find(user => user.Id == id).FirstOrDefault();
+        }
+
+        public User Create(User user)
+        {
+            _collection.InsertOne(user);
+
+            return user;
+        }
+
+        public void Update(string id, User userInput)
+        {
+            _collection.ReplaceOne(user => user.Id == id, userInput);
+        }
+
+        public void Remove(User userInput)
+        {
+            _collection.DeleteOne(user => user.Id == userInput.Id);
+        }
+
+        public void Remove(string id)
+        {
+            _collection.DeleteOne(user => user.Id == id);
+        }
     }
 }
