1efa6c8d659d84f0e85d51f3779813f7c52c343d
Hugo Dias <hmdias@gmail.com>
2020-02-27 23:06:14 +0000

    Add mongodb migration script

---
+++ LightCutAPI/Deploy/001_database_init.js
@@ -0,0 +1,17 @@
+﻿// Create DB
+db = db.getSiblingDB('LIGHTCUT_SGI_DB');
+
+// Create Users collection
+db.createCollection('users');
+
+db.getCollection('users').insert(
+    {
+        "name": "Super User",
+        "email": "hmdias@gmail.com",
+        "password": "super.user",
+        "created_at": ISODate(),
+        "updated_at": null,
+        "deleted_at": null
+    }
+)
+
